45965ec6720b40fc8f0a4d3d3fda45585ef4eef5
LawalAM <lawalaminatmotunrayo@gmail.com>
2023-12-06 13:52:51 +0100

    factored in 0!

--- factorial.js
+++ factorial.js
@@ -1,12 +1,17 @@
 function factorialFunc(n){
   let factorial = 1;
-  for (let i = n; i > 0; i--){
+  if (n == 0 || n == 1){
+    factorial = 1
+  }
+  else if (n > 1){
+    for (let i = n; i > 1; i--){
     factorial = factorial * i
+  }
   }
   console.log(`${n}! is ${factorial}`)
   return factorial
 }
 
-factorialFunc(7)
 factorialFunc(10)
-factorialFunc(6)
+factorialFunc(0)
+factorialFunc(1)
